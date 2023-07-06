48d1f0cfbcaa4a7ec772b7e8e7caf7d10a1d95df
unknown <emilnovikov392@gmail.com>
2023-07-06 18:45:52 +0600

    add widget layout header

--- src/widgets/layout/Header.tsx
+++ src/widgets/layout/Header.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import avatar from '../../../public/icons/avatar.svg';
+import select from '../../../public/icons/select.svg';
 
-const Container = styled.div``
+const Container = styled.div`
+  display: flex;
+  width: 1240px;
+  height: 92px;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+`
 
 const Left = styled.div``
 
@@ -17,16 +25,33 @@ const Logo = styled.a`
   color: var(--blue);
 `
 
-const Right = styled.div``
+const Right = styled.div`
+  display: flex;
+  width: 279px;
+  align-items: center;
+  gap: 15px;
+`
+
+const Text = styled.div`
+  font-family: var(--font);
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+
+  color: var(--black);
+`
 
 const Header: React.FC = () => {
   return (
     <Container>
       <Left>
-        <Logo>Сетевой учебный класс</Logo>
+        <Logo href='/'>Сетевой учебный класс</Logo>
       </Left>
       <Right>
         <img src={avatar} alt="avatar" />
+        <Text>Морозов Антон Дмитриевич</Text>
+        <button><img src={select} alt="меню" /></button>
       </Right>
     </Container>
   )
