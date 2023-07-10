cdaab8846ba3d068f4a510ef8c444e4744bab450
BodySites <100404530+BodySites@users.noreply.github.com>
2023-07-10 21:09:23 +0600

    decomposition of Join page

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./styles.module.css";
 import logo from "../../public/icons/logo.svg";
-import createClass from "../../public/icons/create-class2.svg";
-import search from "../../public/icons/search2.svg";
-import settings from "../../public/icons/settings.svg";
 import topPattern from "../../public/icons/top-pattern.svg";
 import bottomPattern from "../../public/icons/bottom-pattern.svg";
 import monitor from "../../public/icons/monitor.svg";
 import people from "../../public/icons/people.svg";
+import { EnterAccountButton } from "../features/EnterAccount/index.ts";
+import { EnterClassForm } from "../features/EnterClass/index.ts";
+import FunctionsList from "../widgets/FunctionsList/FunctionsList.tsx";
+import functions from "../widgets/FunctionsList/functionsObject.tsx";
 
 const Join: React.FC = () => {
   return (
@@ -16,7 +17,7 @@ const Join: React.FC = () => {
         <header className={styles.header}>
           <img src={logo} alt="Логотип" />
           <div className={styles.titleApp}>Сетевой учебный класс</div>
-          <button className={styles.enterAccount}>Войти</button>
+          <EnterAccountButton />
         </header>
         <main className={styles.main}>
           <div className={styles.titleMotivation}>Учиться стало еще проще!</div>
@@ -24,38 +25,8 @@ const Join: React.FC = () => {
             Комфортная видеосвязь, качественное проведение уроков, контроль
             выполнения заданий – всё это и не только в одном приложении!
           </div>
-          <form
-            action="#"
-            method="get"
-            autoComplete="on"
-            className={styles.formEnter}
-          >
-            <input type="text" name="link" placeholder="Ссылка на класс" />
-            <button type="submit">Присоединиться</button>
-          </form>
-          <div className={styles.functions}>
-            <a href="/lobby" className={styles.function}>
-              <img src={createClass} alt="+" />
-              <div className={styles.titleFunction}>Новый класс</div>
-              <div className={styles.descriptionFunction}>
-                Создать свой класс и провести занятие онлайн
-              </div>
-            </a>
-            <a href="/lobby" className={styles.function}>
-              <img src={search} alt="o" />
-              <div className={styles.titleFunction}>Поиск</div>
-              <div className={styles.descriptionFunction}>
-                Найти класс и получить новые знания
-              </div>
-            </a>
-            <div className={styles.function}>
-              <img src={settings} alt="icon-settings" />
-              <div className={styles.titleFunction}>Настройки</div>
-              <div className={styles.descriptionFunction}>
-                Редактировать свой профиль и классы
-              </div>
-            </div>
-          </div>
+          <EnterClassForm />
+          <FunctionsList functions={functions} />
         </main>
       </div>
       <div className={styles.rightSide}>
--- src/shared/api/models.ts
+++ src/shared/api/models.ts
@@ -1,6 +1,14 @@
 export interface IRoom {
-    id: number,
-    title: string,
-    owner: string,
-    isActive: boolean,
+  id: number;
+  title: string;
+  owner: string;
+  isActive: boolean;
+}
+
+export interface IFunction {
+  icon: string;
+  iconActive: string;
+  title: string;
+  description: string;
+  link: string;
 }
